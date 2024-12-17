3d827261a7bf87507cae2e20532b414ab239a161
Davide131297 <chiffi.davide@gmx.de>
2024-12-17 21:14:17 +0100

    hello test api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('Hello Test');
   res.status(200).json({ message: 'Hello Test' });
 }
