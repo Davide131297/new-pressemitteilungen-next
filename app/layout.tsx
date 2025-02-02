fd8d63149befe77e3aec774498707017bb32b125
Davide131297 <chiffi.davide@gmx.de>
2025-02-02 22:54:50 +0100

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import Head from "next/head";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Pressemitteilungen",
-  description: "Suche und finde Pressemitteilungen",
+  title: 'Pressemitteilungen',
+  description: 'Suche und finde Pressemitteilungen',
 };
 
 export default function RootLayout({
@@ -33,6 +34,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -100,7 +100,7 @@ function NewsContent() {
               color="primary"
             />
           </Stack>
-          <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="grid gap-6 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
             {selectedNews.map((item, index) => (
               <article
                 key={index}
