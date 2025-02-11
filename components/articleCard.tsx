56acbfe09b6912542fd9a03abda966e53de66c06
Davide131297 <chiffi.davide@gmx.de>
2025-02-11 15:39:08 +0100

    News vollständig klickbar statt nur mit einem button

--- components/articleCard.tsx
+++ components/articleCard.tsx
@@ -2,6 +2,7 @@ import { NewsItem } from '@/components/myInterfaces';
 import Image from 'next/image';
 import PriorityHighOutlinedIcon from '@mui/icons-material/PriorityHighOutlined';
 import { sourceImages } from '@/components/sourceImages';
+import Link from 'next/link';
 
 type ArticleCartProps = {
   selectedNews: NewsItem[];
@@ -47,79 +48,73 @@ export default function ArticleCart({ selectedNews }: ArticleCartProps) {
   return (
     <>
       {selectedNews.map((item, index) => (
-        <article
-          key={index}
-          className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow relative flex flex-col justify-between"
-        >
-          <div>
-            <div className="relative">
-              <Image
-                src={
-                  item.urlToImage ||
-                  item.image ||
-                  item.image_url ||
-                  'https://fakeimg.pl/600x400?text=Kein+Bild'
-                }
-                alt={item.title}
-                width={600}
-                height={400}
-                className="w-full h-40 object-cover"
-              />
-              <div
-                className="absolute top-2 right-2 bg-red-500 text-white p-1 rounded-full cursor-pointer w-4 h-4 flex items-center justify-center"
-                onClick={() => alertArticle(item)}
-                title="Melden"
-              >
-                <PriorityHighOutlinedIcon sx={{ fontSize: '12px' }} />
+        <Link key={index} href={item.url || item.link}>
+          <article
+            key={index}
+            className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow relative flex flex-col justify-between"
+          >
+            <div>
+              <div className="relative">
+                <Image
+                  src={
+                    item.urlToImage ||
+                    item.image ||
+                    item.image_url ||
+                    'https://fakeimg.pl/600x400?text=Kein+Bild'
+                  }
+                  alt={item.title}
+                  width={600}
+                  height={400}
+                  className="w-full h-40 object-cover"
+                />
+                <div
+                  className="absolute top-2 right-2 bg-red-500 text-white p-1 rounded-full cursor-pointer w-4 h-4 flex items-center justify-center"
+                  onClick={() => alertArticle(item)}
+                  title="Melden"
+                >
+                  <PriorityHighOutlinedIcon sx={{ fontSize: '12px' }} />
+                </div>
+              </div>
+              <div className="p-4">
+                <h2 className="text-lg font-semibold text-gray-800">
+                  {item.title}
+                </h2>
+                <p className="text-sm text-gray-600 mt-2">
+                  {item.description || 'Keine Beschreibung verfügbar'}
+                </p>
               </div>
             </div>
-            <div className="p-4">
-              <h2 className="text-lg font-semibold text-gray-800">
-                {item.title}
-              </h2>
-              <p className="text-sm text-gray-600 mt-2">
-                {item.description || 'Keine Beschreibung verfügbar'}
-              </p>
+            <div className="p-4 mt-auto">
+              <div className="flex items-center justify-between text-sm text-gray-500">
+                <span>
+                  {item.publishedAt
+                    ? new Date(item.publishedAt).toLocaleDateString()
+                    : item.published_at
+                    ? new Date(item.published_at).toLocaleDateString()
+                    : item.pubDate
+                    ? new Date(item.pubDate).toLocaleDateString()
+                    : item.image_url}
+                </span>
+                <span className="text-xs">
+                  {(() => {
+                    const sourceData = getSourceImage(
+                      item.source as keyof typeof sourceImages
+                    );
+                    return sourceData ? (
+                      <Image
+                        src={sourceData.src}
+                        alt={sourceData.alt}
+                        className="inline-block"
+                        width={sourceData.width}
+                        height={sourceData.height}
+                      />
+                    ) : null;
+                  })()}
+                </span>
+              </div>
             </div>
-          </div>
-          <div className="p-4 mt-auto">
-            <div className="flex items-center justify-between text-sm text-gray-500">
-              <span>
-                {item.publishedAt
-                  ? new Date(item.publishedAt).toLocaleDateString()
-                  : item.published_at
-                  ? new Date(item.published_at).toLocaleDateString()
-                  : item.pubDate
-                  ? new Date(item.pubDate).toLocaleDateString()
-                  : item.image_url}
-              </span>
-              <span className="text-xs">
-                {(() => {
-                  const sourceData = getSourceImage(
-                    item.source as keyof typeof sourceImages
-                  );
-                  return sourceData ? (
-                    <Image
-                      src={sourceData.src}
-                      alt={sourceData.alt}
-                      className="inline-block"
-                      width={sourceData.width}
-                      height={sourceData.height}
-                    />
-                  ) : null;
-                })()}
-              </span>
-            </div>
-            <a
-              href={item.url || item.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block mt-4 text-sky-500 text-sm font-semibold hover:underline"
-            >
-              Artikel lesen
-            </a>
-          </div>
-        </article>
+          </article>
+        </Link>
       ))}
     </>
   );
