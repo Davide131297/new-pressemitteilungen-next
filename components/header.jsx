9c5cb6363553b919e612e1becc5b06e2f500fbef
Davide131297 <chiffi.davide@gmx.de>
2025-09-23 18:09:46 +0200

    header nicht mehr sticky

--- components/header.jsx
+++ components/header.jsx
@@ -3,11 +3,9 @@ import Logo from './logo';
 
 export default function Header() {
   return (
-    <div className="sticky top-0 z-50 bg-white pb-5">
-      <div className="pt-5">
-        <MenuBox />
-        <Logo />
-      </div>
-    </div>
+    <>
+      <MenuBox />
+      <Logo />
+    </>
   );
 }
